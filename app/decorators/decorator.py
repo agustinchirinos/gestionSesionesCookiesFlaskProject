0dec4bb1441a26b55e132ee1cee88944048f9f89
agustin <agustin.iglesias@murciaeduca.es>
2022-03-17 17:36:16 +0100

    Gestión de autorización

---
+++ app/decorators/decorator.py
@@ -0,0 +1,14 @@
+from functools import wraps
+
+from flask_login import current_user
+from werkzeug.exceptions import abort
+
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kws):
+        is_admin = getattr(current_user, 'is_admin', False)
+        if not is_admin:
+            abort(401)
+        return f(*args, **kws)
+    return decorated_function
